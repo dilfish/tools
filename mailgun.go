52604ce1c954c66b2d8c4ea2131f58a20cc842dd
sean <sean@vpn>
2017-12-22 09:59:01 +0800

    add iptables and mailgun

---
+++ mailgun.go
@@ -0,0 +1,26 @@
+package tools
+
+import (
+	"fmt"
+	"gopkg.in/mailgun/mailgun-go.v1"
+)
+
+var ApiKey = ""
+var PubKey = ""
+
+func InitMail(api, pub string) {
+	ApiKey = api
+	PubKey = pub
+}
+
+func SendMail(to, title, content string) error {
+	from := "mc@mg.libsm.com"
+	domain := "mg.libsm.com"
+	mg := mailgun.NewMailgun(domain, ApiKey, PubKey)
+	m := mailgun.NewMessage(
+		from, title,
+		content, to)
+	resp, id, err := mg.Send(m)
+	fmt.Println("id", id, "resp", resp)
+	return err
+}
