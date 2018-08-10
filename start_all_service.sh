19ebef879e6b07feeeb03024d5f61b6af423d9c2
sean <sean@libsm.com>
2018-08-10 16:09:09 +0800

    add mounter

--- start_all_service.sh
+++ start_all_service.sh
@@ -5,5 +5,6 @@ cd /root/go/src/github.com/arthurkiller/shadowsocks-go/cmd/shadowsocks-server
 ./shadowsocks-server -config=config.json 1>1.txt 2>2.txt &
 cd /root/go/src/github.com/dilfish/libsm
 /usr/bin/nohup ./libsm  2 > .libsm.panic & 
+/bin/mount /dev/sdb /disk1
 cd /disk1/cao/91
 /usr/bin/nohup ./dl91go 2 > .dl91go.panic &
