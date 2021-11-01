550799f19559319e5231e9b0b0de737943973a25
SeanZH <i@dilfish.dev>
2021-11-01 18:07:36 +0800

    change text

--- net/block.go
+++ net/block.go
@@ -21,7 +21,8 @@ const BlockHTML = `
 <body>
 <h1>Wechat browser is blocked by x.dilfish.icu team !</h1>
 <h1>Please use desktop or mobile browsers instead</1>
-<h1>不支持微信内部访问，请使用 Chrome/Firefox/Safari/Edge/cURL/NetCat</h1>
+<h1>不支持微信内部访问，请使用 Chrome, Firefox, Safari, Edge, Opera</h1>
+<h1>或者 cURL/NetCat </h1>
 <h1>强者可以不了解弱者，但是弱者必须了解强者才能保护自己 -- <我们无处安放的青春></h1>
 <p><em>Thank you for visiting x.dilfish.icu.</em></p>
 <p><em>Powered by x.dilfish.icu proactive blocking system®©</em></p>
