f395b08d37597cbf7afe9ea852041efa40ea8f4d
SeanZH <i@dilfish.dev>
2022-12-09 17:30:20 +0800

    upgrade io

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/dilfish/tools
 
-go 1.18
-
-require github.com/bits-and-blooms/bitset v1.3.0 // indirect
+go 1.19
--- io/pretty_json.go
+++ io/pretty_json.go
@@ -1,24 +1,4 @@
 package io
 
 import (
-	"encoding/json"
-
-	"github.com/TylerBrock/colorjson"
 )
-
-// PrettyJson is a stupid function
-// you should first self-define a formatter
-// it returned "" when error but no error
-func PrettyJson(obj interface{}) (string, error) {
-	origJs, err := json.Marshal(obj)
-	if err != nil {
-		return "", err
-	}
-	var resp map[string]interface{}
-	err = json.Unmarshal(origJs, &resp)
-	if err != nil {
-		return "", err
-	}
-	str, err := colorjson.Marshal(obj)
-	return string(str), err
-}
